046627fda41317a6a8d1d4a167c66fe03b2010fc
AmandaSnyder <54365463+AmandaSnyder@users.noreply.github.com>
2020-09-20 22:48:46 -0400

    Add files via upload
    defined functions to add to the input. made a test function under the name that will go to calculation ,but the alert doesn't work.

--- HelloWorld/js/logic.js
+++ HelloWorld/js/logic.js
@@ -7,10 +7,16 @@ var calcRun = {
         this.input.trim();
         for (c of this.input) {
             if (c == "0" || c == "1" || c == "2" || c == "3" || c == "4" 
-                    || c == "5" || c = "6" || c = "7" || c = "8" || c = "9"
+                    || c == "5" || c = "6" || c == "7" || c == "8" || c == "9"
                     || c == "+" || c == "-" || c == "x" || c == "/"
-                    || c == "%" || c == "^") 
-            { inClean += c };
+                    || c == "%" || c == "^") { inClean += c };
         }
     }
+    addInput = function(n) {
+        this.input += n;
+    }
+    process = function() {
+        alert("You're getting somewhere!");
+    }
 };
+
